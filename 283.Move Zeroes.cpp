1ab7255c24da30842f4ce6ee88db44f694426242
Sherin saji <146155179+sherinsaji126@users.noreply.github.com>
2024-05-10 17:23:03 +0530

    Create 283.Move Zeroes.cpp

---
+++ 283.Move Zeroes.cpp
@@ -0,0 +1,30 @@
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+*/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
+        for(int i=0,index=0;i<nums.size();i++){
+            if(nums[i]) {
+                swap(nums[i], nums[index]);
+                index++;
+            }
+        }
+        
+    }
+    
+};
